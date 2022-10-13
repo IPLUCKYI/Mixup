5b3378ba335d952775119e675014e3395853b0e4
AxeForward <gamblingdoge@gmail.com>
2022-10-13 16:23:10 +0800

    fixed random seed problem

--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,11 @@
 import numpy as np
 import torch as th
+import random
 
 def to_np(x):
     return x.cpu().detach().numpy()
 
 def set_global_seed(seed:int):
     th.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
